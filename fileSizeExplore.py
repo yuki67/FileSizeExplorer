c746d195f1a3614ba7b16d968a2c0a5b4c7e9a7c
iiyuk <temp>
2016-08-12 21:14:32 +0900

    Initial commit.

---
+++ fileSizeExplore.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+import os
+
+import sys
+import loggingUtility as lgu
+import logging
+import shelve
+from folderInfo import FolderInfo
+
+
+class FileSizeExplore():
+    def __init__(self, dir):
+        lgu.init()
+        self.lookingAt = ''
+
+        try:
+            self.folderInfo = shelve.open('.\\savedShelve\\fileSizeExplore')[dir]
+            print('Successfully opened shelve of ' + dir)
+        except KeyError:
+            print('shelve file for ' + dir + ' doesn\'t exist.\nDo you wont to make new one? [y/n]')
+            response = input()
+            if response == 'y':
+                print('Making file size index. This may take some time...')
+                self.folderInfo = FolderInfo(dir)
+                print('Completed')
+            else:
+                print('exiting...')
+                sys.exit()
+
+        self.showInfo()
+
+        response = input('Do you want to save the result?\n')
+        if response == 'y':
+            self.folderInfo.save(dir)
+            print('Results saved.')
+        else:
+            print('exiting...')
+
+    def showInfo(self):
+        self.folderInfo.show()
+        while(True):
+            key = input('press q to exist.\n')
+            if key == 'q':
+                break
+            else:
+                self.lookingAt = self.connectPath(self.lookingAt, key)
+                logging.debug('now looking at ' + self.lookingAt)
+                try:
+                    self.folderInfo.find(self.lookingAt).show()
+                except KeyError:
+                    print(os.path.join(self.folderInfo.name, self.lookingAt) + ' doesn\'t exist.')
+                    self.lookingAt = self.lookingAt[:-len(key)]
+                    logging.debug('that was wrong. now looking at ' + self.lookingAt)
+                    self.folderInfo.find(self.lookingAt).show()
+        print('done')
+
+    def connectPath(self, cwd, dir):
+        for name in dir.split('\\'):
+            if name == '.':
+                pass
+            elif name == '..':
+                cwd = os.path.split(cwd)[0]
+            else:
+                cwd = os.path.join(cwd, name)
+        return cwd
+
+FileSizeExplore('C:\\')
+
---
+++ folderInfo.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import loggingUtility as lgu
+import logging
+import shelve
+
+
+class FolderInfo():
+    def __init__(self, dir):
+        self.size = 0
+        self.name = os.path.abspath(dir)
+        self.filesSize = 0
+        self.folders = {}
+        self.search(dir)
+
+    def search(self, dir):
+        try:
+            for content in os.listdir(self.name):
+                content = os.path.join(dir, content)
+                # logging.debug('checking content ' + content)
+                if os.path.isfile(content):
+                    # logging.debug(content + ' is a file with size ' + str(os.path.getsize(content)))
+                    self.size += os.path.getsize(content)
+                    self.filesSize += os.path.getsize(content)
+                else:
+                    # logging.debug(content + ' is a folder')
+                    newFolder = FolderInfo(content)
+                    self.folders[os.path.basename(content)] = newFolder
+                    self.size += newFolder.size
+        except PermissionError:
+            logging.debug('Failed to open : ' + dir)
+        except NotADirectoryError:
+            logging.debug('This files doesn\'t seem to exist : ' + dir)
+        # logging.debug('Indexing ended : ' + name)
+
+    def find(self, relPath):
+        if relPath ==  '':
+            return self
+        paths = relPath.split('\\')
+        ans = self
+        for path in paths:
+            ans = ans.folders[path]
+        return ans
+
+    def save(self, dir):
+        os.makedirs('savedShelve', exist_ok = True)
+        shelveFile = shelve.open('.\\savedShelve\\fileSizeExplore')
+        shelveFile[dir] = self
+        shelveFile.close()
+
+    def percentage(self, size, digits=2):
+        return round(size / self.size * 100, 2)
+
+    def show(self):
+        print('----- Contents of ' + self.name + '-----')
+        print('Total size : ' + str(round(self.size / 1024 ** 2, 2)) + 'MB')
+        for folder in self.folders.values():
+            print(str(self.percentage(folder.size)).rjust(5) + '% ' +
+                  str(round(folder.size / 1024 ** 2, 2)).rjust(12) + "MB"
+                  " " + os.path.basename(folder.name))
+        print(str(self.percentage(self.filesSize)).rjust(5) + '% ' +
+              str(round(self.filesSize / 1024 ** 2)).rjust(12) + "MB "
+              ' Files in this directory')
