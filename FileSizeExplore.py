47856cb37b8cc36580738a3c098333b68fca49a2
iiyuk <temp>
2016-12-18 12:23:19 +0900

    FolderInfoの内容を減らした

--- FileSizeExplore.py
+++ FileSizeExplore.py
@@ -4,6 +4,7 @@ FolderInfoの内容を表示
 """
 import os
 import argparse
+import shelve
 from FolderInfo import FolderInfo
 
 
@@ -16,12 +17,44 @@ class FileSizeExplorer():
         self.first_cwd = os.getcwd()
         self.args = self.make_perser().parse_args()
         self.cwd = self.clear_path(self.args.path)
-        if not os.path.exists(self.cwd):
-            print(self.cwd + "does not exist.")
-        else:
-            print("target folder : " + self.cwd)
-            self.info = FolderInfo(self.cwd)
+        self.info = None
+        if os.path.exists(self.cwd):
+            self.prologue()
             self.loop()
+            self.epilogue()
+        else:
+            print(self.cwd + "does not exist.")
+
+    @staticmethod
+    def shelve_exists(key, shelve_path):
+        """
+        pathにsavedShelveがあり、かつその中にkeyというkeyがあるかどうかを返す
+        """
+        if os.path.exists(shelve_path + ".\\savedShelve.dat"):
+            slv = shelve.open(shelve_path + ".\\savedShelve")
+            ans = key in slv.keys()
+            slv.close()
+            return ans
+        else:
+            return False
+
+    def load_shelve(self, key):
+        """
+        savedShelveからkeyを読みだし、selfに代入する
+        """
+        slv = shelve.open("savedShelve")
+        self.info = slv[key]
+        slv.close()
+        return
+
+    def save_shelve(self, directory):
+        """
+        あとから読み出せるようにshelveに保存する。
+       保存先はdirectory\\saved_shelve
+        """
+        slv = shelve.open(directory + "\\savedShelve")
+        slv[self.info.name] = self.info
+        slv.close()
 
     @staticmethod
     def make_perser():
@@ -30,7 +63,7 @@ class FileSizeExplorer():
         返り値に直接parse_arg()することが前提になっている。
         """
         parser = argparse.ArgumentParser(description="Show folder size.")
-        parser.add_argument("-n", dest="path", default=".//", required=False,
+        parser.add_argument("-p", dest="path", default=".//", required=False,
                             action="store", help="Configure folder path. " +
                             "Default : \".\\\"")
         return parser
@@ -38,30 +71,78 @@ class FileSizeExplorer():
     @staticmethod
     def clear_path(path):
         """
-        pathを完全なパス(曖昧さのないパス)にして返す
+        pathを完全なパス(曖昧さのないパス)にして返す。
         """
         path = os.path.expanduser(path)
         path = os.path.expandvars(path)
         return os.path.abspath(path)
 
+    def prologue(self):
+        """
+        開始時のプロンプト。
+        """
+        print("target folder : " + self.cwd)
+        while True:
+            if self.shelve_exists(self.cwd, os.getcwd()):
+                response = input(
+                    "Saved shelve found. Road this shelve? [y/n] : ")
+                if response == "y":
+                    print("loading...")
+                    self.load_shelve(self.cwd)
+                    return
+                elif response == "n":
+                    break
+            else:
+                print("No saved shelve found.")
+                break
+        print("Collectiong imformation about %s."
+              " This may take some time." % self.cwd)
+        self.info = FolderInfo(self.cwd)
+        print("You can reduce errors by running script as administrater.")
+
     def epilogue(self):
         """
         終了時のプロンプト。
         """
-        response = input('Do you want to save the result?[y/n]:')
-        if response == 'y':
-            self.info.save_shelve(self.first_cwd)
-            print('Results saved.')
-        else:
-            print('exiting...')
+        while True:
+            response = input("Do you want to save the result?[y/n] : ")
+            if response == "y":
+                self.save_shelve(self.first_cwd)
+                print("Results saved.")
+                break
+            elif response == "n":
+                print("exiting...")
+                break
+
+    def show(self, path):
+        """
+        pathフォルダの情報(そのフォルダのサイズとサブフォルダのサイズ)を返す。
+        """
+        temp_size = self.info.size
+        temp_size_files = self.info.size_files
+        temp_sub_dirs = self.info.sub_dirs
+        rel_path = path.replace(self.info.name, "")
+        # rel_path == ""のときpath = self.name
+        if rel_path != "":
+            for path in rel_path.split("\\"):
+                temp_size = temp_sub_dirs[path].size
+                temp_size_files = temp_sub_dirs[path].size_files
+                temp_sub_dirs = temp_sub_dirs[path].sub_dirs
+
+        result = {}
+        result[temp_size_files] = "Files in this directory"
+        for sub_dir in temp_sub_dirs.values():
+            result[sub_dir.size] = ".\\" + os.path.basename(sub_dir.name)
+
+        return result, temp_size
 
     def show_info(self):
         """
         self.infoとself.cwdに基づいてフォルダの内容を表示する。
         """
-        result, total_size = self.info.show(self.cwd)
-        print('----- Contents of ' + self.cwd + '-----')
-        print('Total size : ' + str(round(total_size / 1024 ** 2, 2)) + 'MB')
+        result, total_size = self.show(self.cwd)
+        print("----- Contents of " + self.cwd + "-----")
+        print("Total size : " + str(round(total_size / 1024 ** 2, 2)) + "MB")
         for size, name in sorted(result.items(), reverse=True):
             print("%5.2f%% %10.2fMB %s" %
                   (size / total_size * 100, size / 1048576, name))
@@ -73,8 +154,8 @@ class FileSizeExplorer():
         os.chdir(self.cwd)
         self.show_info()
         while True:
-            key = input('press q to exist.\n')
-            if key == 'q':
+            key = input("press q to exist.\n")
+            if key == "q":
                 break
             key = self.clear_path(key)
             if os.path.exists(key):
@@ -86,7 +167,6 @@ class FileSizeExplorer():
             else:
                 print("Path " + key + " does not exist.")
             self.show_info()
-        print('done')
-        self.epilogue()
+        print("done")
 
 FileSizeExplorer()
--- FolderInfo.py
+++ FolderInfo.py
@@ -1,103 +1,30 @@
 # -*- coding: utf-8 -*-
-"""
-フォルダーのサイズを保持する。
-"""
 
 import os
-import shelve
 
 
 class FolderInfo():
-    """
-    フォルダーについての情報を保持する。
-    関数の引数が有効なパスかどうかを判定しない。
-    """
 
     def __init__(self, directory):
         self.size = 0
         self.size_files = 0
         self.name = directory
         self.sub_dirs = {}
-        self.construct()
-
-    def construct(self):
-        """
-        フォルダの要素(=ファイルorフォルダ)をスキャンし、注目しているフォルダ(=self.name)のサイズを求める。
-        """
-        if self.shelve_exists(self.name):
-            self.load_shelve(self.name)
-        else:
-            try:
-                for content in os.listdir(self.name):
-                    content = self.name + ".\\" + content
-                    if os.path.isfile(content):
-                        # contentがファイルのとき
-                        self.size += os.path.getsize(content)
-                        self.size_files += os.path.getsize(content)
-                    else:
-                        # contentがフォルダのとき
-                        basename = os.path.basename(content)
-                        self.sub_dirs[basename] = FolderInfo(content)
-                        self.size += self.sub_dirs[basename].size
-            except PermissionError:
-                print('Failed to open : ' + self.name)
-            except FileNotFoundError:
-                print('Failed to open : ' + self.name)
-            except NotADirectoryError:
-                print('This file doesn\'t exist : ' + self.name)
-
-    @staticmethod
-    def shelve_exists(name):
-        """
-        cwdにsavedShelveがあり、かつその中にnameというkeyがあるかどうかを返す
-        """
-        if os.path.exists("savedShelve.dat"):
-            slv = shelve.open('savedShelve')
-            ans = name in slv.keys()
-            slv.close()
-            return ans
-        else:
-            return False
-
-    def load_shelve(self, name):
-        """
-        savedShelveからnameを読みだし、selfに代入する
-        """
-        slv = shelve.open('savedShelve')
-        self.size = slv[name].size
-        self.size_files = slv[name].size_files
-        self.name = slv[name].name
-        self.sub_dirs = slv[name].sub_dirs
-        slv.close()
-        return self
-
-    def save_shelve(self, directory):
-        """
-        あとから読み出せるようにshelveに保存する。
-        ディレクトリはdirectory\\saved_shelve
-        """
-        slv = shelve.open(directory + "\\savedShelve")
-        slv[self.name] = self
-        slv.close()
-
-    def show(self, path):
-        """
-        pathフォルダの情報(そのフォルダのサイズとサブフォルダのサイズ)を返す。
-        """
-        temp_size = self.size
-        temp_size_files = self.size_files
-        temp_sub_dirs = self.sub_dirs
-        rel_path = path.replace(self.name, "")[1:]
-        # rel_path == ""のときpath = self.name
-        if rel_path != "":
-            for path in rel_path.split("\\"):
-                temp_size = temp_sub_dirs[path].size
-                temp_size_files = temp_sub_dirs[path].size_files
-                temp_sub_dirs = temp_sub_dirs[path].sub_dirs
-
-        result = {}
-        result[temp_size_files] = "Files in this directory"
-        for sub_dir in temp_sub_dirs.values():
-            result[sub_dir.size] = ".\\" + os.path.basename(sub_dir.name)
-
-        return result, temp_size
+        try:
+            for content in os.listdir(self.name):
+                content = self.name + ".\\" + content
+                if os.path.isfile(content):
+                    # contentがファイルのとき
+                    self.size += os.path.getsize(content)
+                    self.size_files += os.path.getsize(content)
+                else:
+                    # contentがフォルダのとき
+                    basename = os.path.basename(content)
+                    self.sub_dirs[basename] = FolderInfo(content)
+                    self.size += self.sub_dirs[basename].size
+        except PermissionError:
+            print('Failed to open : ' + self.name)
+        except FileNotFoundError:
+            print('Failed to open : ' + self.name)
+        except NotADirectoryError:
+            print('This file doesn\'t exist : ' + self.name)
